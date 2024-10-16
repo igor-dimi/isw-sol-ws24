e59f33e42487344e4ee4b6f5ab472c4d508c61d5
Igor Dimitrov <igor-dimi@protonmail.com>
2024-10-16 16:09:46 +0200

    arbeitsblatt 01 workfiles

---
+++ bltr/arb/01/app/src/main/java/de/uni/hd/isw/ab01/HelloWorld.java
@@ -0,0 +1,7 @@
+package de.uni.hd.isw.ab01;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world\n");
+    }
+}
---
+++ bltr/arb/01/helloworld2/src/main/java/de/uni/hd/isw/helloworld2/HelloWorld.java
@@ -0,0 +1,5 @@
+package de.uni.hd.isw.helloworld2;
+
+public class HelloWorld {
+    public static void main(String[] args) {System.out.println("hey there!");}
+}
