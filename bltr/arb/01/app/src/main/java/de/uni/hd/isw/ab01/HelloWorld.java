49707612c16a602304c272a9cdc867f0acda1a1a
Igor Dimitrov <igor-dimi@protonmail.com>
2024-10-25 11:38:44 +0200

    deleted bltr folder

--- bltr/arb/01/app/src/main/java/de/uni/hd/isw/ab01/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package de.uni.hd.isw.ab01;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("hello world\n");
-    }
-}
--- bltr/arb/01/helloworld2/src/main/java/de/uni/hd/isw/helloworld2/HelloWorld.java
+++
@@ -1,5 +0,0 @@
-package de.uni.hd.isw.helloworld2;
-
-public class HelloWorld {
-    public static void main(String[] args) {System.out.println("hey there!");}
-}
